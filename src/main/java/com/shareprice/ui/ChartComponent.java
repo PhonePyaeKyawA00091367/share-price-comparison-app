85f214cc42732f1a9e426dd57fe90e2a24de998e
PhonePyaeKyawA00091367 <phonepyae@roehampton.ac.uk>
2026-02-27 15:10:18 +0000

    Update ChartComponent.java

--- src/main/java/com/shareprice/ui/ChartComponent.java
+++ src/main/java/com/shareprice/ui/ChartComponent.java
@@ -1,1 +1,13 @@
+package com.shareprice.ui;
 
+import com.shareprice.model.Price;
+import java.util.List;
+
+public interface ChartComponent {
+
+    void renderSingleChart(List<Price> prices, String title);
+
+    void renderComparisonChart(List<Price> pricesA, List<Price> pricesB, String title);
+
+    void clearChart();
+}

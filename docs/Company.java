a7675ddd21ac1b6d970087778bc730c4bc8155e0
Vijohnson S <vijohnson207@gmail.com>
2026-03-19 23:13:34 +0000

    Add Company class with symbol and name attributes

---
+++ docs/Company.java
@@ -0,0 +1,11 @@
+package entities;
+
+public class Company {
+    private String symbol;
+    private String name;
+
+    public Company(String symbol, String name) {
+        this.symbol = symbol;
+        this.name = name;
+    }
+}

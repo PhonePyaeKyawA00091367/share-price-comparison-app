3ffdf0d0b9291740fa975c2a43bd6d5de8167ac7
Vijohnson S <vijohnson207@gmail.com>
2026-03-19 23:13:49 +0000

    Add DateRange class for start and end dates

---
+++ docs/DateRange.java
@@ -0,0 +1,11 @@
+package entities;
+
+public class DateRange {
+    private String start;
+    private String end;
+
+    public DateRange(String start, String end) {
+        this.start = start;
+        this.end = end;
+    }
+}
